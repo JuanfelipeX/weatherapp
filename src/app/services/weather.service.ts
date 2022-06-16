40c97400c3dd315edd4098b54a922b52e86e9c83
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-16 16:34:56 -0500

    getWeather in weather service

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
+  apiKey: string = '1b150657042d53ab6b4a088363d79a73';
+  URI: string = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.URI = `https://api.openweathermap.org/data/2.5/weather?appid=${this.apiKey}&units=metric&q=`;
+  }
+
+  getWeather(cityName: string, countryCode: string) {
+    return this.http.get(`${this.URI}${cityName},${countryCode}`);
+  }
 }
